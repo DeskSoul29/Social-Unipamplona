af4b0df91e686af0e742f8ca411dc3e81468b510
desksoul29 <jhannavarro2001@gmail.com>
2022-06-07 18:46:37 -0500

    Registros funcionando, Login con problemas de envio de datos

--- src/components/SignUpForm/SignUpForm.js
+++ src/components/SignUpForm/SignUpForm.js
@@ -1,116 +1,129 @@
-import React,{useState} from "react";
-import { Row,Col,Form,Button,Spinner} from "react-bootstrap";
-import {values,size } from "lodash";
+import React, { useState } from "react";
+import { Row, Col, Form, Button, Spinner } from "react-bootstrap";
+import { values, size } from "lodash";
 //import {toast} from "react-toastify";
-import { ToastContainer, toast } from 'react-toastify';
-import {isEmailValid} from "../../utils/validations";
+import { toast } from "react-toastify";
+import { isEmailValid } from "../../utils/validations";
 import { signUpApi } from "../../api/auth";
 
 import "./SignUpForm.scss";
 
 export default function SignUpForm(props) {
-    const {setShowModal }=props;
-    const [formData, setFormData] = useState(initialFormValue());
-    const [signUpLoading, setSignUpLoading] = useState(false);
+  const { setShowModal } = props;
+  const [formData, setFormData] = useState(initialFormValue());
+  const [signUpLoading, setSignUpLoading] = useState(false);
 
-    const onSubmit = e => {
-        e.preventDefault();
-        //console.log(formData);
+  const onSubmit = (e) => {
+    e.preventDefault();
 
-        let validCount = 0;
-        values(formData).some(value => 
-            { value && validCount++;
-        return null; 
+    let validCount = 0;
+    values(formData).some((value) => {
+      value && validCount++;
+      return null;
     });
-   // console.log(validCount);
-    //console.log(size(formData));
 
-    if(validCount !== size(formData)){
-        toast.warning("campos incompletos");
+    if (validCount !== size(formData)) {
+      toast.warning("Campos incompletos");
     } else {
-        if (!isEmailValid(formData.email)){
-            toast.warning("email invalido")
-
-        } else 
-        if (FormData.password !== FormData.repeatPassword){
-            toast.warning("las contraseñas no son iguales");
-
-        } else if (size(formData.password) < 6){
-            toast.warning("contraseñas debe tener 6 caracteres")
-
-        } else {
-            setSignUpLoading(true);
-            signUpApi(formData);
-
-            //toast.success("campos compleos");
-        }
-    
+      if (!isEmailValid(formData.email)) {
+        toast.warning("Email invalido");
+      } else if (FormData.password !== FormData.repeatPassword) {
+        toast.warning("Las contraseñas no son iguales");
+      } else if (size(formData.password) < 6) {
+        toast.warning("Contraseñas debe tener 6 caracteres");
+      } else {
+        setSignUpLoading(true);
+        signUpApi(formData)
+          .then((response) => {
+            if (response.code) {
+              toast.warning(response.message);
+            } else {
+              toast.success("El registro ha sido correcto");
+              setShowModal(false);
+              setFormData(initialFormValue());
+            }
+          })
+          .catch(() => {
+            toast.error("Error del servidor, inténtelo más tarde!");
+          })
+          .finally(() => {
+            setSignUpLoading(false);
+          });
+      }
     }
-       
-    };
-    //formularios construidos solo con inputs
+  };
 
-    //Función reutilizable
-    const onChange = e => {
-        setFormData({...formData, [e.target.name]: e.target.value});
-    };
+  const onChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
 
-
-
-    return (
+  return (
     <div className="sign-up-form">
-        <h2>Crea tu cuenta</h2>
-        <Form onSubmit={onSubmit} onChange={onChange}>
-            <Form.Group>
-                <Row>
-                    <Col>
-                        <Form.Control 
-                        type="text" 
-                        placeholder="Nombre" 
-                        name="nombre"
-                        defaultValue={formData.nombre} 
-                        />
-                    </Col>
-                    <Col>
-                        <Form.Control 
-                        type="text" 
-                        placeholder="Apellidos" 
-                        name="apellidos"
-                        defaultValue={formData.apellidos} 
-                         />
-                    </Col>
-                </Row>
-            </Form.Group>
-            <Form.Group>
-                <Form.Control type="email" placeholder="Correo electronico" name="email" defaultValue={formData.email}  />
-            </Form.Group>
-            <Form.Group>
-                <Row>
-                    <Col>
-                        <Form.Control type="password" placeholder="Contraseña"  name="password" defaultValue={formData.password} />
-                    </Col>
-                    <Col>
-                        <Form.Control type="text" placeholder="Repetir contraseña" name="repeatPassword" defaultValue={formData.repeatPassword} />
-                    </Col>
-                </Row>
-            </Form.Group>
+      <h2>Crea tu cuenta</h2>
+      <Form onSubmit={onSubmit} onChange={onChange}>
+        <Form.Group>
+          <Row>
+            <Col>
+              <Form.Control
+                type="text"
+                placeholder="Nombre"
+                name="nombre"
+                defaultValue={formData.nombre}
+              />
+            </Col>
+            <Col>
+              <Form.Control
+                type="text"
+                placeholder="Apellidos"
+                name="apellidos"
+                defaultValue={formData.apellidos}
+              />
+            </Col>
+          </Row>
+        </Form.Group>
+        <Form.Group>
+          <Form.Control
+            type="email"
+            placeholder="Correo electronico"
+            name="email"
+            defaultValue={formData.email}
+          />
+        </Form.Group>
+        <Form.Group>
+          <Row>
+            <Col>
+              <Form.Control
+                type="password"
+                placeholder="Contraseña"
+                name="password"
+                defaultValue={formData.password}
+              />
+            </Col>
+            <Col>
+              <Form.Control
+                type="password"
+                placeholder="Repetir contraseña"
+                name="repeatPassword"
+                defaultValue={formData.repeatPassword}
+              />
+            </Col>
+          </Row>
+        </Form.Group>
 
-
-            <Button variant="primary" type="submit">
-                {!signUpLoading ? "Registrate" : <Spinner animation = "border"/>}
-                
-            </Button>
-        </Form>
+        <Button variant="primary" type="submit">
+          {!signUpLoading ? "Registrate" : <Spinner animation="border" />}
+        </Button>
+      </Form>
     </div>
-    );
+  );
 }
 
-function initialFormValue(){
-    return{
-        nombre:"",
-        apellidos:"",
-        email:"",
-        password:"",
-        repeatPassword:""
-    };
+function initialFormValue() {
+  return {
+    nombre: "",
+    apellidos: "",
+    email: "",
+    password: "",
+    repeatPassword: "",
+  };
 }
--- src/page/SingInSingUp/SingInSingUp.js
+++ src/page/SingInSingUp/SingInSingUp.js
@@ -1,85 +1,87 @@
 import React, { useState } from "react";
-import {Container,Row,Col,Button} from "react-bootstrap";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import { 
-    faSearch,
-    faUser,
-    faComment
-} from '@fortawesome/free-solid-svg-icons';
+import { Container, Row, Col, Button } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch, faUser, faComment } from "@fortawesome/free-solid-svg-icons";
 import BasicModal from "../../components/Modal/BasicModal";
 import SignUpForm from "../../components/SignUpForm";
-import logoblue from "../../assets/png/logo.png";
+import SignInForm from "../../components/SignInForm";
+import logoUP from "../../assets/png/illustration.png";
 import logowhite from "../../assets/png/logo-white.png";
 
 import "./SingInSingUp.scss";
 
-export default function SingInSingUp(){
-    const [showModal, setShowModal] = useState(false);
-    const [contentModal,setContentModal]= useState(null);
-    const openModal = content => {
-        setShowModal(true);
-        setContentModal(content); 
-        
-    };
-    return(
-        <>
-       <Container className="signin-singup" fluid>
-           <Row>
-               <LeftComponent/>
-               <RightComponent openModal={openModal} setShowModal={setShowModal}/>
-           </Row>
-       </Container>
-        <BasicModal show={showModal} setShow={setShowModal}>
-            {contentModal}
+export default function SingInSingUp(props) {
+  const { setRefreshCheckLogin } = props;
+  const [showModal, setShowModal] = useState(false);
+  const [contentModal, setContentModal] = useState(null);
 
-        </BasicModal>
-      
-       </>
-    );
+  const openModal = (content) => {
+    setShowModal(true);
+    setContentModal(content);
+  };
+
+  return (
+    <>
+      <Container className="signin-singup" fluid>
+        <Row>
+          <LeftComponent />
+          <RightComponent
+            openModal={openModal}
+            setShowModal={setShowModal}
+            setRefreshCheckLogin={setRefreshCheckLogin}
+          />
+        </Row>
+      </Container>
+      <BasicModal show={showModal} setShow={setShowModal}>
+        {contentModal}
+      </BasicModal>
+    </>
+  );
 }
-function LeftComponent(){
-    return(
-        <Col className="signin-singup__left" xs={6}>
-            
-            <img src={logoblue} alt="socialunipamplona"/>
-            <div>
-                <h2>
-                    <FontAwesomeIcon icon={faSearch} />
-                    sigue lo que te interesa.
-                </h2>
-                <h2>
-                    <FontAwesomeIcon icon={faUser} />
-                    Enterate de que esta hablando en unipamplona.</h2>
-                <h2>
-                    <FontAwesomeIcon icon={faComment} />
-                    Únete a la conversación.
-                </h2> 
-            </div>
-        </Col>
-    );
+function LeftComponent() {
+  return (
+    <Col className="signin-singup__left" xs={6}>
+      <img src={logoUP} alt="SocyUP" />
+      {/* <div>
+        <h2>
+          <FontAwesomeIcon icon={faSearch} />
+          Sigue lo que te interesa.
+        </h2>
+        <h2>
+          <FontAwesomeIcon icon={faUser} />
+          Enterate de que se esta hablando en Unipamplona.
+        </h2>
+        <h2>
+          <FontAwesomeIcon icon={faComment} />
+          Únete a la conversación.
+        </h2>
+      </div> */}
+    </Col>
+  );
 }
 
 function RightComponent(props) {
-    const {openModal, setShowModal}=props;
-    return(
-        <Col className="signin-singup__right" xs={6}>
-            <div>
-            <img src={logowhite} alt="socialunipamplona"/>
-                <h2>Mira los que esta pasando en el mundo en este momento</h2>
-                <h3>únete a socialunipamplona</h3>
-                <Button 
-                    variant="primary" 
-                    onClick={() => openModal(<SignUpForm setShowModal={setShowModal}/>)}>  
-                Regístrate  
-                </Button>
-                <Button 
-                    variant="outline-primary" 
-                    onClick={()=> openModal(<h2>Formulario de Login</h2>)}
-                    >
-                      Iniciar sesión 
-                </Button>
-
-            </div>
-        </Col>
-    );
+  const { openModal, setShowModal } = props;
+  return (
+    <Col className="signin-singup__right" xs={6}>
+      <div>
+        <img src={logowhite} alt="SocyUP" />
+        <h2>Mira lo que esta pasando en la Unipamplona en este momento</h2>
+        <h3>¡Únete a SocyUP!</h3>
+        <Button
+          variant="primary"
+          onClick={() => openModal(<SignUpForm setShowModal={setShowModal} />)}
+        >
+          Regístrate
+        </Button>
+        <Button
+          variant="outline-primary"
+          onClick={() => openModal(<SignInForm />)}
+          //    setRefreshCheckLogin={setRefreshCheckLogin
+        >
+          Iniciar sesión
+        </Button>
+      </div>
+    </Col>
+  );
 }
